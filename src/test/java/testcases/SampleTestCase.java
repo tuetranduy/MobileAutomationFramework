cff3ac2db0c6557e0d862813e009959b2e1d059b
Tue Tran Duy <tuetranduy@gmail.com>
2021-05-24 21:24:50 +0700

    add test listener - allure report and take screenshot

--- src/test/java/testcases/SampleTestCase.java
+++ src/test/java/testcases/SampleTestCase.java
@@ -1,7 +1,10 @@
 package testcases;
 
+import listeners.TestExecutionListener;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(TestExecutionListener.class)
 public class SampleTestCase extends BaseTest {
 
     @Test
